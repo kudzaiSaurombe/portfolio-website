6f5e4f438e888d7057adf8422fadd489569129f2
kudzaiSaurombe <82084840+kudzaiSaurombe@users.noreply.github.com>
2024-07-28 19:16:58 +0200

    fixing white contact text on white background tile for contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,7 +73,7 @@ const Contact = () => {
                 className='flex-[0.75] bg-white dark:bg-black-100 p-8 rounded-2xl'
             >
                 <p className={`${styles.sectionSubText} text-black dark:text-white`}>Get in touch</p>
-                <h3 className={`${styles.sectionHeadText} text-black-100 dark:text-white`}>Contact.</h3>
+                <h3 className={`${styles.sectionHeadText} text-black-200 dark:text-white`}>Contact.</h3>
 
                 <form
                     ref={formRef}
